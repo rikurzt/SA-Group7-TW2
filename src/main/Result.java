95f154f81aa25210179866293180fa7a39d6c79b
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 15:34:24 +0800

    Result

---
+++ src/main/Result.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.time.LocalDateTime;
+
+public class Result {
+    LocalDateTime time;
+    boolean result;
+    public Result(LocalDateTime time, boolean result){
+        this.time = time;
+        this.result = result;
+    }
+}
