62684ac87d238ead33ee4e4856d228533e1d3acc
rikun <rikurzt@gmail.com>
2022-05-06 15:52:04 +0800

    resolve some merge error

--- src/main/CloudComputing.java
+++ src/main/CloudComputing.java
@@ -1,11 +1,19 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class CloudComputing {
-    public int re(){
-        return 2;
-    }
+    private ai  aicom= new ai();
+    private DataBaseMananger db = new DataBaseMananger();
+    private DataProcessing dataprocessing = new DataProcessing();
+    public static Stack<UploadFile> DataBuffer = new Stack<UploadFile>();
+
+
 }
- class DataBaseMananger{
+class DataBaseMananger{
+    public List FakeDataBase = new ArrayList<>();
     public void savetodatabase(){
 
     }
